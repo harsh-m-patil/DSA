8e8e9bc78ff29f49a3413f1027b6a49819201668
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2025-04-03 16:04:13 +0530

    solve flatten binary tree

---
+++ binary-trees/FlattenTree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  public void flatten(TreeNode root) {
+    List<TreeNode> pre = new ArrayList<>();
+    dfs(root, pre);
+
+    for (int i = 0; i < pre.size() - 1; i++) {
+      pre.get(i).right = pre.get(i + 1);
+      pre.get(i).left = null;
+    }
+  }
+
+  public static void dfs(TreeNode root, List<TreeNode> pre) {
+    if (root == null) {
+      return;
+    }
+
+    pre.add(root);
+    dfs(root.left, pre);
+    dfs(root.right, pre);
+  }
+}
